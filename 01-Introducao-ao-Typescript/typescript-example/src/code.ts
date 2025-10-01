33f1cf16ca9d06c0da4f648392d8a745bd87dfa2
Icaro Gabriel <164355795+IcaroGabrielIcaro@users.noreply.github.com>
2025-10-01 07:54:58 -0300

    feat: Adição da explicacao da aula de introducao ao typescript

--- 01-Introducao-ao-Typescript/typescript-example/src/code.ts
+++ 01-Introducao-ao-Typescript/typescript-example/src/code.ts
@@ -1,3 +1,20 @@
+// Código javascript que vai rodar o código sem precisar instalar nada
+
+// let animal = 'cavalo'
+// switch(animal) {
+//     case 'vaca': console.log('A vaca foi selecionada.')
+//     break;
+//     case 'cavalo': console.log('O cavalo foi selecionado.')
+//     break;
+//     default:
+//         console.log('Animal nao detectado.')        
+// }
+
+// Para rodar um código typescript é necessario instalar o typescript
+// Use o comando:
+// npm install -g ts-node
+// ts-node src/code.ts -> ao executar esse comando ele vai rodar o codigo
+
 let animal : string;
 animal = 'cavalo';
 
