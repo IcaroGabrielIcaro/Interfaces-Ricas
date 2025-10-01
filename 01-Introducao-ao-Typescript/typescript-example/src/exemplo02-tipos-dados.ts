ad7d028b9f2d6c35f266e65af44af599e6cbd758
Icaro Gabriel <164355795+IcaroGabrielIcaro@users.noreply.github.com>
2025-10-01 08:14:11 -0300

    feat: Introdução aos tipos de dados no typescript

---
+++ 01-Introducao-ao-Typescript/typescript-example/src/exemplo02-tipos-dados.ts
@@ -0,0 +1,5 @@
+let numero = 5
+
+numero = numero + animale // ele reclama com isso porque isso é realmente incorreto no typescript
+
+console.log(numero) // isso o javacript deixa, o typescript nao permite (tem que ser tipado)
