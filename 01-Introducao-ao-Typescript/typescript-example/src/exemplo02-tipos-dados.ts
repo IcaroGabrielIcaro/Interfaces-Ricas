b082541483beaaa578f2efa807a39833493d3c48
Icaro Gabriel <164355795+IcaroGabrielIcaro@users.noreply.github.com>
2025-10-07 19:12:41 -0300

    Delete 01-Introducao-ao-Typescript directory

--- 01-Introducao-ao-Typescript/typescript-example/src/exemplo02-tipos-dados.ts
+++
@@ -1,5 +0,0 @@
-let numero = 5
-
-numero = numero + animale // ele reclama com isso porque isso é realmente incorreto no typescript
-
-console.log(numero) // isso o javacript deixa, o typescript nao permite (tem que ser tipado)
