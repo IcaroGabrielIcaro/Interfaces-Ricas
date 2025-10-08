36851bea1a302d19ea3ff35012ff127e168b61d2
Icaro Gabriel <164355795+IcaroGabrielIcaro@users.noreply.github.com>
2025-10-08 07:09:58 -0300

    Delete ambiente-typescript directory

--- ambiente-typescript/src/quadrado-elementos.test.ts
+++
@@ -1,9 +0,0 @@
-import { quadradoFor, quadradoForEach } from './quadrado-elementos.js';
-
-test('1a - quadrado For simples', () => {
-    expect(quadradoFor([1,2,3,4,5])).toEqual([1,4,9,16,25]);
-});
-
-test('2a - quadrado For Each', () => {
-    expect(quadradoForEach([1,2,3,4,5])).toEqual([1,4,9,16,25]);
-});
