3e7ab9de6990484386bf1c69457ee95dc78edf45
Icaro Gabriel <164355795+IcaroGabrielIcaro@users.noreply.github.com>
2025-10-15 08:25:02 -0300

    Update app.ts

--- hello/src/app/app.ts
+++ hello/src/app/app.ts
@@ -1,17 +1,57 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
+/**
+ * Um componente é tudo o que estamos vendo na tela inicial
+ * 
+ * Todo componente é uma classe que tem o decorador @Component
+ * Normalmente quando cria pelo angular é criado 3 arquivos separados: o componente
+ * O template da pagina a ser renderizado que está sob responsabilidade do componente
+ * E a folha de estilo
+ * Os outros 2 apps nao tem relacao com o Componente app
+ * O arquivo obrigatorio é o app.ts */
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule],
   // templateUrl: './app.html', // template pela url
   styleUrl: './app.css',
   // template in line
   template: `
-    <h1>{{ title() }}</h1>
+    <!-- Titulo é uma função -->
+    <main>
+      <h1>Hello, {{ variavel }}</h1>
+      <p>Congratulations! Your {{ title() }} is running</p>
+      <p-button label="Check" />
+      <input type="button" value="Check2">
+      
+      <!-- <div class="pill-group">
+        @for (item of items; track item.title) {
+          <a class="pill" [href]="item.link" target="_blank" rel="noopener">
+            <span>{{ item.title }}</span>
+          </a>
+          }
+        </div> -->
+        
+      </main>
     <router-outlet></router-outlet>
   `
 })
 export class App {
-  protected readonly title = signal('hello');
+  // A definição de uma variavel com signal para ajudar na programacao funcional
+  // variavel que serve para guardar um valor que ao mesmo tempo é uma funcao
+  // Toda vez que vai alterar o valor ou algo vai poder mexer como em um uma variavel
+  // Mas se for ler, tem que ler como uma funcao
+  protected readonly title = signal('Título');
+
+  protected readonly variavel: string = 'ícaro'; // nao é signal
+
+  protected readonly items = [
+      { title: 'Explore the Docs', link: 'https://angular.dev' },
+      { title: 'Learn with Tutorials', link: 'https://angular.dev/tutorials' },
+      { title: 'Prompt and best practices for AI', link: 'https://angular.dev/ai/develop-with-ai'},
+      { title: 'CLI Docs', link: 'https://angular.dev/tools/cli' },
+      { title: 'Angular Language Service', link: 'https://angular.dev/tools/language-service' },
+      { title: 'Angular DevTools', link: 'https://angular.dev/tools/devtools' },
+  ];
 }
