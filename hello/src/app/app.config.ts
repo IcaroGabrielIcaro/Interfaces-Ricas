ee7ef44b3341aa800b674e3037cbd5605fc2fa33
Icaro Gabriel <164355795+IcaroGabrielIcaro@users.noreply.github.com>
2025-10-15 08:24:13 -0300

    Update app.config.ts

--- hello/src/app/app.config.ts
+++ hello/src/app/app.config.ts
@@ -1,5 +1,8 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'; // Adiciona essa linha
+import { providePrimeNG } from 'primeng/config'; // Adiciona essa linha para falar o provedor de prime NG
+import Aura from '@primeuix/themes/aura'; // Adiciona o aura
 
 import { routes } from './app.routes';
 
@@ -7,6 +10,12 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes), // fala os provedores de fato
+    provideAnimationsAsync(),
+    providePrimeNG({
+        theme: {
+            preset: Aura
+        }
+    })
   ]
 };
