40cadc1ef2850f575282bd998f6ac58927ab43c1
Icaro Gabriel <icarogabrielpc05@gmail.com>
2025-10-19 17:32:24 -0300

    feat: Adiciona botao para excluir e atualizar filme

--- MyMovies/src/app/components/movie-card/movie-card.ts
+++ MyMovies/src/app/components/movie-card/movie-card.ts
@@ -1,52 +1,29 @@
-import { Component} from '@angular/core';
-
-interface Movie {
-  id: number;
-  nome: string;
-  review: string;
-  nota: number;
-  dataAssistida: Date;
-  favorito: boolean;
-}
-
+import { Component, inject} from '@angular/core';
+import { Movie, MovieEntity } from '../../service/movie';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-movie-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './movie-card.html',
   styleUrl: './movie-card.css'
 })
 
 
 export class MovieCard {
-  movies: Movie[] = [
-    {
-      id: 1,
-      nome: 'Interstellar',
-      review: 'Uma jornada emocionante pelo espaço e tempo.',
-      nota: 9.5,
-      dataAssistida: new Date('2024-05-10'),
-      favorito: true
-    },
-    {
-      id: 2,
-      nome: 'Oppenheimer',
-      review: 'Um retrato intenso da mente por trás da bomba atômica.',
-      nota: 9,
-      dataAssistida: new Date('2024-08-02'),
-      favorito: false
-    },
-    {
-      id: 3,
-      nome: 'Duna: Parte Dois',
-      review: 'Visual deslumbrante e narrativa épica.',
-      nota: 8.7,
-      dataAssistida: new Date('2024-11-18'),
-      favorito: false
-    }
-  ];
+  movieService: Movie = inject(Movie);
+  movies: MovieEntity[] = this.movieService.listarTodos();
 
-  toggleFavorito(movie: Movie) {
+  toggleFavorito(movie: MovieEntity) {
     movie.favorito = !movie.favorito;
   }
+
+  deletarFilme(id: number) {
+    const confirmacao = confirm('Tem certeza que deseja excluir este filme?');
+    if (confirmacao) {
+      this.movieService.remover(id);
+      this.movies = this.movieService.listarTodos();
+    }
+  }
+
 }
