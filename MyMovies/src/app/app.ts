5bd39e26d1dfd88501d79ca333347e06ba6d7b7f
Icaro Gabriel <icarogabrielpc05@gmail.com>
2025-10-19 10:46:53 -0300

    feat: Adiciona componentes no app principal

--- MyMovies/src/app/app.ts
+++ MyMovies/src/app/app.ts
@@ -1,13 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { Header } from './components/header/header';
+import { MovieCard } from './components/movie-card/movie-card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet, ButtonModule, Header, MovieCard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
   protected readonly title = signal('MyMovies');
 }
