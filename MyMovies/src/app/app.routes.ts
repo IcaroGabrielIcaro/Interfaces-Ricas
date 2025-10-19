72ac0f16da2f7eaac344521ceb3ebabc697a77b1
Icaro Gabriel <icarogabrielpc05@gmail.com>
2025-10-19 17:33:00 -0300

    feat: Adiciona sistema de rotas

--- MyMovies/src/app/app.routes.ts
+++ MyMovies/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MovieCard } from './components/movie-card/movie-card';
+import { MovieForm } from './components/movie-form/movie-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'filmes', component: MovieCard},
+    { path: 'filmes/novo', component: MovieForm},
+    { path: 'filmes/editar/:id', component: MovieForm},
+];
--- MyMovies/src/app/app.ts
+++ MyMovies/src/app/app.ts
@@ -2,11 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { Header } from './components/header/header';
-import { MovieCard } from './components/movie-card/movie-card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule, Header, MovieCard],
+  imports: [RouterOutlet, ButtonModule, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
