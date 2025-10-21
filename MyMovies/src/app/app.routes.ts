be2020c9ce65b26984e2094f26c1b365c91c6c55
Icaro Gabriel <icarogabrielpc05@gmail.com>
2025-10-20 22:22:25 -0300

    refactor: Adiciona rota de home

--- MyMovies/src/app/app.routes.ts
+++ MyMovies/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MovieCard } from './components/movie-card/movie-card';
 import { MovieForm } from './components/movie-form/movie-form';
+import { Home } from './components/home/home';
 
 export const routes: Routes = [
+    { path: '', component: Home},
     { path: 'filmes', component: MovieCard},
     { path: 'filmes/novo', component: MovieForm},
     { path: 'filmes/editar/:id', component: MovieForm},
--- MyMovies/src/app/app.ts
+++ MyMovies/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { Header } from './components/header/header';
+import { Sidebar } from './components/sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule, Header],
+  imports: [RouterOutlet, ButtonModule, Header, Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
