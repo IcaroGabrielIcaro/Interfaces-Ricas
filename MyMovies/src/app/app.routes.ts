b4bf81d835e03ee6a38f3e6ca2ed71575762d845
Icaro Gabriel <icarogabrielpc05@gmail.com>
2025-10-21 22:09:38 -0300

    refactor: Prepara tela principal para lidar com modal de criacao

--- MyMovies/src/app/app.routes.ts
+++ MyMovies/src/app/app.routes.ts
@@ -1,11 +1,10 @@
 import { Routes } from '@angular/router';
 import { MovieCard } from './components/movie-card/movie-card';
-import { MovieForm } from './components/movie-form/movie-form';
 import { Home } from './components/home/home';
+import { MovieDetail } from './components/movie-detail/movie-detail';
 
 export const routes: Routes = [
     { path: '', component: Home},
     { path: 'filmes', component: MovieCard},
-    { path: 'filmes/novo', component: MovieForm},
-    { path: 'filmes/editar/:id', component: MovieForm},
+    { path: 'filmes/:id', component: MovieDetail},
 ];
--- MyMovies/src/app/app.ts
+++ MyMovies/src/app/app.ts
@@ -3,14 +3,23 @@ import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { Header } from './components/header/header';
 import { Sidebar } from './components/sidebar/sidebar';
+import { MovieForm } from './components/movie-form/movie-form';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule, Header, Sidebar],
+  imports: [RouterOutlet, ButtonModule, Header, Sidebar, MovieForm],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 
 export class App {
-  protected readonly title = signal('MyMovies');
+  showMovieForm = false;
+
+  abrirModal() {
+    this.showMovieForm = true;
+  }
+
+  fecharModal() {
+    this.showMovieForm = false;
+  }
 }
