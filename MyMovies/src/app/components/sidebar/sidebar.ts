5a2d6b1dbcf79a69f47ed3381804368e6c933f3b
Icaro Gabriel <icarogabrielpc05@gmail.com>
2025-10-21 22:06:43 -0300

    refactor: Ajusta sidebar para poder criar um novo filme

--- MyMovies/src/app/components/sidebar/sidebar.ts
+++ MyMovies/src/app/components/sidebar/sidebar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -8,5 +8,9 @@ import { RouterLink } from '@angular/router';
   styleUrl: './sidebar.css'
 })
 export class Sidebar {
+  @Output() abrirFormulario = new EventEmitter<void>();
 
+  abrirForm() {
+    this.abrirFormulario.emit();
+  }
 }
