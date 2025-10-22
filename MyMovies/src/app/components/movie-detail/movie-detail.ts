2c5479645f42d6278e31eebdb74019d59ea17140
Icaro Gabriel <icarogabrielpc05@gmail.com>
2025-10-21 22:06:17 -0300

    feat: Adiciona detalhamento do filme

---
+++ MyMovies/src/app/components/movie-detail/movie-detail.ts
@@ -0,0 +1,42 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Movie, MovieEntity } from '../../service/movie';
+import { MovieForm } from '../movie-form/movie-form';
+
+@Component({
+  selector: 'app-movie-detail',
+  standalone: true,
+  imports: [CommonModule, MovieForm],
+  templateUrl: './movie-detail.html',
+})
+export class MovieDetail {
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+  private movieService = inject(Movie);
+
+  movie: MovieEntity | undefined;
+  showUpdateForm = false;
+
+  ngOnInit() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.movie = this.movieService.listarTodos().find((m) => m.id === id);
+  }
+
+  abrirAtualizacao() {
+    this.showUpdateForm = true;
+  }
+
+  fecharAtualizacao() {
+    this.showUpdateForm = false;
+  }
+
+  deletarFilme() {
+    if (confirm('Tem certeza que deseja deletar este filme?')) {
+      if (this.movie) {
+        this.movieService.remover(this.movie.id);
+        this.router.navigate(['/']);
+      }
+    }
+  }
+}
