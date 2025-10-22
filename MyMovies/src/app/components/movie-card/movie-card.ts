dc4aeedecad2013c90f9cb022e3388761815c592
Icaro Gabriel <icarogabrielpc05@gmail.com>
2025-10-21 22:07:30 -0300

    refactor: Modificando a aparencia do card do filme

--- MyMovies/src/app/components/movie-card/movie-card.ts
+++ MyMovies/src/app/components/movie-card/movie-card.ts
@@ -1,18 +1,23 @@
-import { Component, inject} from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Movie, MovieEntity } from '../../service/movie';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-movie-card',
+  standalone: true,
   imports: [RouterLink],
   templateUrl: './movie-card.html',
   styleUrl: './movie-card.css'
 })
-
-
-export class MovieCard {
+export class MovieCard implements OnInit {
   movieService: Movie = inject(Movie);
-  movies: MovieEntity[] = this.movieService.listarTodos();
+  movies: MovieEntity[] = [];
+
+  ngOnInit(): void {
+    this.movies = this.movieService
+      .listarTodos()
+      .sort((a, b) => b.id - a.id);
+  }
 
   toggleFavorito(movie: MovieEntity) {
     movie.favorito = !movie.favorito;
@@ -22,8 +27,9 @@ export class MovieCard {
     const confirmacao = confirm('Tem certeza que deseja excluir este filme?');
     if (confirmacao) {
       this.movieService.remover(id);
-      this.movies = this.movieService.listarTodos();
+      this.movies = this.movieService
+        .listarTodos()
+        .sort((a, b) => b.id - a.id);
     }
   }
-
 }
