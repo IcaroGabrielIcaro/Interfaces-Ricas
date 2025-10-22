d828cf318e8b96d418e5c7c634f677c72880111f
Icaro Gabriel <icarogabrielpc05@gmail.com>
2025-10-21 22:08:59 -0300

    refactor: Ajusta listagem ds filmes para poder passar para o lado em carrossel

--- MyMovies/src/app/components/home-movie-list/home-movie-list.ts
+++ MyMovies/src/app/components/home-movie-list/home-movie-list.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MovieEntity } from '../../service/movie';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home-movie-list',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home-movie-list.html',
 })
 export class HomeMovieList {
